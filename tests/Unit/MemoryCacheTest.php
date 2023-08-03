9f3774e9e7e8ea35c29e3533efe821d01d4c973c
Yhann Daniel <yhanndaniel@gmail.com>
2023-08-03 13:33:35 -0300

    test: Make MemoryCache Tests

---
+++ tests/Unit/MemoryCacheTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\Cache\MemoryCache;
+use Tests\TestCase;
+
+class MemoryCacheTest extends TestCase
+{
+
+    private $memoryCache;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->memoryCache = new MemoryCache();
+    }
+    public function test_get_cache()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 4);
+
+
+
+        $this->assertEquals(['foo' => 'bar'], $this->memoryCache->get('foo'));
+    }
+
+    public function test_ttl_cache_works()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 1);
+
+        sleep(2);
+
+        $this->assertEquals([], $this->memoryCache->get('foo'));
+    }
+
+    public function test_get_cache_not_found()
+    {
+        $this->assertEquals([], $this->memoryCache->get('foo'));
+    }
+
+    public function test_set_cache()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 10);
+
+        $this->assertArrayHasKey('foo', $this->memoryCache->getAll());
+    }
+
+    public function test_delete_cache()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 10);
+
+        $this->memoryCache->delete('foo');
+
+        $this->assertArrayNotHasKey('foo', $this->memoryCache->getAll());
+    }
+
+    public function test_flush_cache()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 10);
+
+        $this->memoryCache->flush();
+
+        $this->assertEquals([], $this->memoryCache->getAll());
+    }
+
+    public function test_has_cache()
+    {
+        $this->memoryCache->set('foo', ['foo' => 'bar'], 10);
+
+        $this->assertTrue($this->memoryCache->has('foo'));
+        $this->assertFalse($this->memoryCache->has('bar'));
+    }
+
+
+}
